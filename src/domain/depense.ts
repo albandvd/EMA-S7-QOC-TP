18021d41136e180c4b81f58bb197ee1f814b05ce
albandvd <alban.david06@gmail.com>
2026-01-12 17:23:34 +0100

    Feat: add Depense model

---
+++ src/domain/depense.ts
@@ -0,0 +1,28 @@
+export class Address {
+  depenseId?: string;
+  description: string;
+  montant: string;
+  userId: string;
+  magasin: string;
+  date: Date;
+
+  constructor(
+    description: string,
+    montant: string,
+    userId: string,
+    magasin: string,
+    date: Date,
+    depenseId?: string
+  ) {
+    this.depenseId = depenseId;
+    this.description = description;
+    this.montant = montant;
+    this.userId = userId;
+    this.magasin = magasin;
+    this.date = date;
+  }
+
+    getDepenseDetails(): string {
+    return `${this.userId} - ${this.montant} - ${this.magasin} - ${this.date.toDateString()}`;
+  }
+}
