6c081f730475fe320a45a3e5ab6637048a939b4a
albandvd <alban.david06@gmail.com>
2026-01-13 11:24:52 +0100

    Feat: create User service

--- src/ports/driven/repoPort.ts
+++ src/ports/driven/repoPort.ts
@@ -1,7 +1,16 @@
 import { Address } from '../../domain/address';
+import { User } from '../../domain/user';
 
 export interface AddressRepositoryPort {
   findAll(): Promise<Address[]>;
   findById(id: string): Promise<Address | null>;
   save(address: Omit<Address, 'id'>): Promise<Address>;
+}
+
+export interface UserRepositoryPort {
+  findAll(): Promise<User[]>;
+  findById(id: string): Promise<User | null>;
+  save(user: Omit<User, 'userId'>): Promise<User>;
+  modify(user: User): Promise<User>;
+  delete(id: string): Promise<void>;
 }
---
+++ src/services/userService.ts
@@ -0,0 +1,27 @@
+import { User } from '../domain/user';
+import { UserRepositoryPort } from '../ports/driven/repoPort';
+import { UserPort } from "../ports/driving/userPort";
+
+export class UserService implements UserPort {
+  constructor(private repo: UserRepositoryPort) {}
+
+  async listUsers(): Promise<User[]> {
+     return this.repo.findAll();
+  }
+
+  async getUser(id: string): Promise<User | null> {
+    return this.repo.findById(id);
+  }
+
+  async createUser(input: Omit<User, 'userId'>): Promise<User> {
+    return this.repo.save(input);
+  }
+
+  async updateUser(input: User): Promise<User> {
+    return this.repo.modify(input);
+  }
+
+  async deleteUser(id: string): Promise<void> {
+    return this.repo.delete(id);
+  }
+}
