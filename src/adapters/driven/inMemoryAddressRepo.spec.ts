883a8856f22ae40873ffc0be2d3a0021f36c0bf4
albandvd <alban.david06@gmail.com>
2026-01-13 09:45:25 +0100

    fix: update

---
+++ src/adapters/driven/inMemoryAddressRepo.spec.ts
@@ -0,0 +1,32 @@
+import { Address } from '../../domain/address';
+import { AddressRepositoryPort } from '../../ports/driven/repoPort';
+import { v4 as uuidv4 } from 'uuid';
+import { InMemoryAddressRepo } from './inMemoryAddressRepo';
+
+describe('inMemoryAddressRepo', () => {
+  let repo: InMemoryAddressRepo;
+  let addresses: Address[] = [];
+
+  beforeEach(async () => {
+    addresses = [];
+    repo = new InMemoryAddressRepo(addresses);
+  })
+
+  it('should save an address', async () => {
+    const addressData = new Address('123 Main St','Anytown',12345);
+    const savedAddress = await repo.save(addressData);
+
+    expect(savedAddress).toHaveProperty('id');
+    expect(savedAddress.street).toBe(addressData.street);
+    expect(addresses.length).toBe(1);
+  });
+
+  it('should get all addresses by duplicating variable', async () => {
+    addresses = [new Address('123 Main St','Anytown',12345)];
+    repo = new InMemoryAddressRepo(addresses);
+    const allAddresses = await repo.findAll();
+
+    expect(allAddresses).toEqual(addresses);
+    expect(allAddresses).not.toBe(addresses); // Ensure it's a copy
+  });
+})
--- src/adapters/driven/inMemoryAddressRepo.ts
+++ src/adapters/driven/inMemoryAddressRepo.ts
@@ -2,21 +2,21 @@ import { Address } from '../../domain/address';
 import { AddressRepositoryPort } from '../../ports/driven/repoPort';
 import { v4 as uuidv4 } from 'uuid';
 
-const store: Address[] = [];
-
 export class InMemoryAddressRepo implements AddressRepositoryPort {
+  constructor(private readonly store: Address[] = []) {}
+
   async findAll(): Promise<Address[]> {
-    return store.slice();
+    return this.store.slice();
   }
 
   async findById(id: string): Promise<Address | null> {
-    const found = store.find((s) => s.id === id);
+    const found = this.store.find((s) => s.id === id);
     return found ?? null;
   }
 
   async save(address: Omit<Address, 'id'>): Promise<Address> {
     const newAddress: Address = { id: uuidv4(), ...address };
-    store.push(newAddress);
+    this.store.push(newAddress);
     return newAddress;
   }
 }
