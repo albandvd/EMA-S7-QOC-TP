5c2fa27853c4918988d32ad9fa4285678f3091d5
albandvd <alban.david06@gmail.com>
2026-01-14 12:09:09 +0100

    feat: add cercle, user and depense to the api

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,29 @@ import * as fs from "node:fs";
 import * as YAML from 'yaml';
 import swaggerUi from 'swagger-ui-express';
 
-import { AddressController } from './adapters/driving/addressController';
 import { InMemoryAddressRepo } from "./adapters/driven/inMemoryAddressRepo";
 import { AddressService } from "./services/addressService";
+import { AddressController } from './adapters/driving/addressController';
+
+import { InMemoryCercleRepo } from './adapters/driven/inMemoryCercleRepo';
+import { CercleService } from './services/cercleService';
+import { CercleController } from './adapters/driving/cercleController';
+
+import { InMemoryUserRepo } from './adapters/driven/inMemoryUserRepo';
+import { UserService } from './services/userService';
+import { UserController } from './adapters/driving/userController';
+
+import { InMemoryDepenseRepo } from './adapters/driven/inMemoryDepenseRepo';
+import { DepenseService } from './services/depenseService';
+import { DepenseController } from './adapters/driving/depenseController';
 
 const app = express();
 app.use(express.json());
 
 const addressRepo = new InMemoryAddressRepo();
+const cercleRepo = new InMemoryCercleRepo();
+const userRepo = new InMemoryUserRepo();
+const depenseRepo = new InMemoryDepenseRepo();
 
 const file  = fs.readFileSync('./openapi.yaml', 'utf8')
 const swaggerDocument = YAML.parse(file)
@@ -19,8 +34,19 @@ const swaggerDocument = YAML.parse(file)
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const addressService = new AddressService(addressRepo);
+const cercleService = new CercleService(cercleRepo);
+const userService = new UserService(userRepo);
+const depenseService = new DepenseService(depenseRepo);
+
 const addressController = new AddressController(addressService);
+const cercleController = new CercleController(cercleService);
+const userController = new UserController(userService);
+const depenseController = new DepenseController(depenseService);
+
 addressController.registerRoutes(app);
+cercleController.registerRoutes(app);
+userController.registerRoutes(app);
+depenseController.registerRoutes(app);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
